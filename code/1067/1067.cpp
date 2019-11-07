e9d2d541c82a8afc1bff3129b6ada50803b0772a
hushengquan <1390305506@qq.com>
2019-11-07 10:41:37 +0800

    .

--- code/1067/1067.cpp
+++ code/1067/1067.cpp
@@ -1,1 +1,27 @@
 #include <iostream>
+using namespace std;
+
+int main() {
+    int n, count = 0;
+    string pw, input;
+    cin >> pw >> n;
+    getchar();
+
+    while (1) {
+        getline(cin, input);
+        if (input == "#") break;
+        count++;
+        if (count <= n && input == pw) {
+            cout << "Welcome in" << endl;
+            break;
+        } else if (count <= n && input != pw) {
+            cout << "Wrong password: " << input << endl;
+            if (count == n) {
+                cout << "Account locked" << endl;
+                break;
+            }
+        }
+    }
+    return 0;
+}
+
